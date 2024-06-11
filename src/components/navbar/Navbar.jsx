dff0d229968ccb89b498f216a577a584ce9f3015
ntp-bi <bintp22122002@gmail.com>
2024-06-11 21:53:10 +0700

    push navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
+
+import "./navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="navbarContainer">
+            <div className="navbarLeft">
+                <span className="logo">i@mBi</span>
+            </div>
+            <div className="navbarCenter">
+                <div className="searchbar">
+                    <Search className="searchIcon"/>
+                    <input
+                        placeholder="Search for friend, post or video"
+                        className="searchInput"
+                    />
+                </div>
+            </div>
+            <div className="navbarRight">
+                <div className="navbarLinks">
+                    <span className="navbarLink">Homepage</span>
+                    <span className="navbarLink">Timeline</span>
+                </div>
+                <div className="narbarIcons">
+                    <div className="navbarIconItem">
+                        <Person />
+                        <span className="navbarIconBadge">1</span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <Chat />
+                        <span className="navbarIconBadge">2</span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <Notifications />
+                        <span className="navbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="" className="navbarImg" />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
