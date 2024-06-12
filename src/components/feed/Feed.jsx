ebf6e14a57f6a59b56cfa9f38a7583b331422a47
ntp-bi <bintp22122002@gmail.com>
2024-06-12 10:21:43 +0700

    push make post components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Share from "../../components/share/Share";
 import "./feed.css";
+import Post from "../post/Post";
 
 const Feed = () => {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
             </div>
         </div>
     );
