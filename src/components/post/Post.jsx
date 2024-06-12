f1cf26ae2858484a9c181d21b836badeaf48fb6c
ntp-bi <bintp22122002@gmail.com>
2024-06-12 16:16:44 +0700

    push make likeHandler func

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { MoreVert } from "@mui/icons-material";
 import { Users } from "../../dummyData";
 import "./post.css";
 
 const Post = ({ post }) => {
+    const [like, setLike] = useState(post.like);
+    const [isLiked, setIsLiked] = useState(false);
+
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1);
+        setIsLiked(!isLiked);
+    };
+
     return (
         <div className="post">
             <div className="postWrapper">
@@ -32,11 +40,19 @@ const Post = ({ post }) => {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="/assets/like.png" alt="" />
-                        <img className="likeIcon" src="/assets/heart.png" alt="" />
-                        <span className="postLikeCounter">
-                            {post.like} people liked it
-                        </span>
+                        <img
+                            className="likeIcon"
+                            src="/assets/like.png"
+                            onClick={likeHandler}
+                            alt=""
+                        />
+                        <img
+                            className="likeIcon"
+                            src="/assets/heart.png"
+                            alt=""
+                            onClick={likeHandler}
+                        />
+                        <span className="postLikeCounter">{like} people liked it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">{post.comment} comments</span>
